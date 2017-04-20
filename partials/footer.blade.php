c0c4dbe35a86659c80a7d15714fab6b3c1e00c5c
Nipurn Doshi <nipunhere@gmail.com>
2017-04-20 10:11:22 -0700

    Adding basic theme for chpc

---
+++ partials/footer.blade.php
@@ -0,0 +1,56 @@
+
+<footer class="uu-footer">
+            <div class="container">
+               <div class="row"><!-- FOOTER AREA 1 -->
+<div class="col-md-3">
+  <h2>Where We Are</h2>
+  <p><a href="http://map.utah.edu/index.html?code=inscc">The Center For High Performance Computing</a><br><a href="http://map.utah.edu/index.html?code=inscc"> 155 S 1452 E, RM. 405</a><br><a href="http://map.utah.edu/index.html?code=inscc"> SLC, UT 84112-0190</a><br> 801.585.3791<br> <a href="/about/contact.php">Contact</a></p>
+	<p>© 2017 <a href="http://www.utah.edu">The University of Utah</a></p>
+</div>
+<!-- END FOOTER AREA 1 -->
+
+<!-- FOOTER AREA 2 -->
+<div class="col-md-3">
+	<h2>Other Links</h2>
+	<ul>
+		<li><a href="http://www.utah.edu/nondiscrimination/">Nondiscrimination &amp; Accessibility</a></li>
+		<li><a href="http://www.utah.edu/disclaimer/">Disclaimer</a></li>
+		<li><a href="http://www.utah.edu/privacy/">Privacy</a></li>
+		<li><a href="http://www.utah.edu/credits.php">Credits &amp; Attributions</a></li>
+		<li><span id="directedit">
+         <a id="de" href="http://a.cms.omniupdate.com/10?skin=utah&amp;account=utah_home&amp;site=chpc&amp;action=de&amp;path=/index.pcf">Last Updated: 4/13/17</a>
+      </span></li>
+	</ul>
+</div>
+<!-- END FOOTER AREA 2 -->
+
+<!-- FOOTER AREA 3 -->
+<div class="col-md-3">
+  <h2>Stay Connected</h2>
+  
+		
+		<div class="socons">
+			<ul>
+				<li><a href="https://gate.acs.utah.edu/" class="cis-link" title="CIS login">CIS</a></li>
+				<li><a href="https://www.umail.utah.edu/" class="umail-link" title="U-Mail">U-Mail</a></li>
+				<li><a class="facebook-link" title="facebook" href="http://www.facebook.com/universityofutah">facebook</a></li>
+        		<li><a class="twitter-link" title="twitter" href="http://twitter.com/uutah">twitter</a></li>
+			</ul>
+		</div>
+  
+</div>
+<!-- END FOOTER AREA 3 -->
+
+<!-- FOOTER AREA 4 -->
+<div class="col-md-3">
+	
+		
+		<div class="footer-logo">		
+			<a href="http://imagineu.utah.edu/"><img src="//templates.utah.edu/_main-v2/_images/footer/imagine_u.png" alt="Imagine U: The University of Utah"></a>
+		</div>	
+	
+</div>
+<!-- END FOOTER AREA 4 -->
+</div>
+            </div>
+         </footer>
---
+++ partials/template.blade.php
@@ -0,0 +1,98 @@
+<div class="col-md-12 chpc-top-banner">
+	<!--
+	<h1>Hero Image</h1>
+	<h2>This is a simple hero unit, a simple jumbotron-style component for calling extra 7
+	attention to featured content or information.</h2>
+	<p><a class="btn btn-primary btn-large">Learn more</a></p>
+	-->
+</div>
+
+
+<div class="col-md-12 breathing-space">
+
+	<div class="row">
+		<div class="col-md-offset-2 col-md-2">
+			<a href="{{ URL::to('/') }}/login"><h4>Login</h4></a><hr/>
+			<a href="{{ URL::to('/') }}/create"><h4>Register</h4></a><hr/>
+
+			<div class="panel">
+                                 <div class="panel-body panel-bg-light">
+                                    <h2>System Status</h2>	<div>last update: 04/20/17 9:51 am</div>
+	<table class="table table-condensed">
+			<tbody><tr>
+			<th colspan="3">General Nodes</th>
+		</tr>
+		<tr>
+			<td>system</td>
+			<td>procs</td>
+			<td>% util.</td>
+		</tr>
+						<tr>
+					<td>ember</td>
+					<td colspan="2">Status Unavailable</td>
+				</tr>
+								<tr>
+					<td>kingspeak</td>
+					<td>880/880</td>
+					<td>100%</td>
+				</tr>
+								<tr>
+					<td>lonepeak</td>
+					<td>256/256</td>
+					<td>100%</td>
+				</tr>
+						<tr>
+			<th colspan="3">Restricted Nodes</th>
+		</tr>
+		<tr>
+			<td>system</td>
+			<td>procs</td>
+			<td>% util.</td>
+		</tr>
+						<tr>
+					<td>ash</td>
+					<td>7356/7396</td>
+					<td>99.46%</td>
+				</tr>
+								<tr>
+					<td>apexarch</td>
+					<td colspan="2">Status Unavailable</td>
+				</tr>
+								<tr>
+					<td>ember</td>
+					<td colspan="2">Status Unavailable</td>
+				</tr>
+								<tr>
+					<td>kingspeak</td>
+					<td>6212/6816</td>
+					<td>91.14%</td>
+				</tr>
+								<tr>
+					<td>lonepeak</td>
+					<td colspan="2">Status Unavailable</td>
+				</tr>
+				</tbody></table>                                    
+                                 </div>
+                              </div>
+		</div>
+		
+		<div class="col-md-6 scigap-info">
+			<h1 >Why Supercomputing?</h1>
+			<p>
+				In addition to deploying and operating high performance computational resources and providing advanced user support and training, CHPC serves as an expert team to broadly support the increasingly diverse research computing needs on campus. These needs include support for big data, big data movement, data analytics, security, virtual machines, Windows science application servers, protected environments for data mining and analysis of protected health information, and advanced networking.
+			</p>
+			<h2>Cluster Utilization</h2>
+                                    <p><a href="/usage/graphs.php?g=cluster%20utilization&amp;host=combined&amp;type=daily_utilization"><img src="https://winkler.chpc.utah.edu/cacti/graph_image.php?action=view&amp;local_graph_id=6208&amp;rra_id=6" alt=""></a></p>
+                                    <p><a href="/usage/graphs.php?g=cluster%20utilization&amp;host=combined&amp;type=daily_utilization"><img src="https://winkler.chpc.utah.edu/cacti/graph_image.php?action=view&amp;local_graph_id=6209&amp;rra_id=6" alt=""></a></p>
+                                    <p><a href="/usage/graphs.php?g=cluster%20utilization&amp;host=combined&amp;type=daily_utilization"><img src="https://winkler.chpc.utah.edu/cacti/graph_image.php?action=view&amp;local_graph_id=6923&amp;rra_id=6" alt="" width="595" height="211"></a></p>
+
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-offset-2 col-md-8">
+			
+
+		</div>
+	</div>
+</div>
